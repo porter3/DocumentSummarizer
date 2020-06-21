3b9649715fc4b2a44eca411a5eaa41daba2aacf8
porter3 <jakeporter310@gmail.com>
2020-06-21 12:21:41 -0400

    remove test method, add logger/replace println()

--- src/main/java/com/jakeporter/DocumentAnalyzer/controllers/FileController.java
+++ src/main/java/com/jakeporter/DocumentAnalyzer/controllers/FileController.java
@@ -1,26 +1,22 @@
 package com.jakeporter.DocumentAnalyzer.controllers;
 
-import com.jakeporter.DocumentAnalyzer.exceptions.FileStorageException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.jakeporter.DocumentAnalyzer.service.FileService;
 
 @Controller
 public class FileController {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     FileService fileService;
-
-    @GetMapping("/")
-    public ResponseEntity<String> test() {
-        System.out.println("This is being accessed!!!!!!!!!!");
-        return new ResponseEntity<>("Hi", HttpStatus.OK);
-    }
 
     // File will be removed from memory after request has been processed
     @PostMapping("/upload")
@@ -31,7 +27,7 @@ public class FileController {
         } catch (Exception e) {
             return new ResponseEntity<>("Error " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        System.out.println("File uploaded");
-        return new ResponseEntity<>("Sucess!", HttpStatus.OK);
+        logger.info("File uploaded");
+        return new ResponseEntity<>("Success!", HttpStatus.OK);
     }
 }
