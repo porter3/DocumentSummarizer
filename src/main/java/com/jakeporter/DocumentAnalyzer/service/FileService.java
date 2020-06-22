cd7a48009ccd971254eb73588bfd818ab747799e
porter3 <jakeporter310@gmail.com>
2020-06-22 13:13:05 -0400

    change env variable name for file path, log uploadDirectory, add summarize method (currently just text extraction)

--- src/main/java/com/jakeporter/DocumentAnalyzer/service/FileService.java
+++ src/main/java/com/jakeporter/DocumentAnalyzer/service/FileService.java
@@ -1,6 +1,10 @@
 package com.jakeporter.DocumentAnalyzer.service;
 
 import com.jakeporter.DocumentAnalyzer.exceptions.FileStorageException;
+import com.jakeporter.DocumentAnalyzer.utilities.DocumentSummarizer;
+import com.jakeporter.DocumentAnalyzer.utilities.PythonSummarizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -16,20 +20,27 @@ import java.nio.file.StandardCopyOption;
 @Service
 public class FileService {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     // value can be set by com.jakeporter.DocumentAnalyzer.app.upload.dir in application.properties - defaults to user.home if not defined
-    @Value("${com.jakeporter.DocumentAnalyzer.app.upload.dir:${user.home}}")
+    @Value("${upload.dir:${user.home}}")
     public String uploadDirectory;
 
     public void uploadFile(MultipartFile file) throws FileStorageException {
-
         try {
             // create absolute path of the file and normalize it for different OSs
             Path copyLocation = Paths.get(uploadDirectory + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+            logger.info("Upload directory: " + uploadDirectory);
             // copy the file's input stream to the path and replace any file with the same name
             Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
             throw new FileStorageException("Error storing " + file.getOriginalFilename() + ".");
         }
+    }
+
+    public String summarize(MultipartFile file) throws IOException {
+        DocumentSummarizer summarizer = new PythonSummarizer();
+        return summarizer.summarizeDocument(file);
     }
 }
