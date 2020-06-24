52b995e335c575beac35480b0855b861698faca9
porter3 <jakeporter310@gmail.com>
2020-06-24 16:23:13 -0400

    add find_average_score, generate_summary, threshold_multiplier variable. Complete first version of script

--- src/main/resources/textSummarizer.py
+++ src/main/resources/textSummarizer.py
@@ -3,7 +3,14 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import PorterStemmer
 
 # random generated paragraph from randomwordgenerator.com
-text = "He took a sip of the drink. He wasn't sure whether he liked it or not, but at this moment it didn't matter. She had made it especially for him so he would have forced it down even if he had absolutely hated it. That's simply the way things worked. She made him a new-fangled drink each day and he took a sip of it and smiled, saying it was excellent."
+# text = "He took a sip of the drink. He wasn't sure whether he liked it or not, but at this moment it didn't matter. She had made it especially for him so he would have forced it down even if he had absolutely hated it. That's simply the way things worked. She made him a new-fangled drink each day and he took a sip of it and smiled, saying it was excellent."
+text = """One of the greatest aspects of NLP is that is stretches across multiple areas of computational studies from artificial intelligence to computational linguistics all studying the interactions between computers and the language of humans. It is primarily concerned with programming computers to accurately and quickly process large amounts of natural language corpora. What are natural language corpora? It is the study of language as expressed by real-world languages. It is a comprehensive approach to understanding a set of abstract rules from a text and the relationship that language has with another.
+While NLP has become even more present with the computer industrial revolution in the modern era it was actually the brain child of the amazing Alan Turing who along with helping to crack the German Enigma Coding machine also wrote an article titled “Computing Machinery and Intelligence” which proposed the first serious use of relating humans language to computers. With the ever present nature of technology in our daily lives we have seen the emergence of just how influential NLP can be to our daily lives through revolutionary tools like Google Translate, IBM Watson, Speech Recognition and generation and sentiment analysis.
+However, like all things there are a few areas of concern and disadvantages of NLP. It struggles to generate language that would naturally flow as a person talks similar to when you read a bad movie script and it sounds like a computer talking. While there are methods to attempt to understand changes in tone NLP continues to struggle with understanding things like sarcasm and detecting things like humor. However, this is an area of much study that I look forward to the day that the "sarcasm breakthrough" occurs. If for nothing else, then to better understand the occasional text or instant message from a friend."""
+
+# threshold is the score a sentence has to pass to make it into the summary
+threshold_multiplier = 1
+
 
 def create_frequency_table(text) -> dict:
     # generally speaking, stop words are filler words (https://en.wikipedia.org/wiki/Stop_words)
@@ -50,10 +57,35 @@ def score_sentences(sentences, frequency_table) -> dict:
     return sentence_scores
 
 
+# find average score of the sentences to use as a threshold
+def find_average_score(sentence_scores) -> int:
+    sum = 0
+    for entry in sentence_scores:
+        sum += sentence_scores[entry]
+
+    average = int(sum / len(sentence_scores))
+
+    return average
+
+
+def generate_summary(sentences, sentence_scores, threshold) -> str:
+    summary = ''
+
+    for sentence in sentences:
+        abbrv_sentence = sentence[:10]
+        if abbrv_sentence in sentence_scores and sentence_scores[abbrv_sentence] > threshold:
+            summary += ' ' + sentence
+
+    return summary
+
+
 def main():
     frequency_table = create_frequency_table(text)
     sentences = sent_tokenize(text)
     sentence_scores = score_sentences(sentences, frequency_table)
+    threshold = find_average_score(sentence_scores)
+    summary = generate_summary(sentences, sentence_scores, threshold * threshold_multiplier)
+    print(summary)
 
 
 if __name__ == '__main__':
