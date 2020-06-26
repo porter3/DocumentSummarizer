b3e3894852a3a27cfd9211add9dff2dbe3837f14
porter3 <jakeporter310@gmail.com>
2020-06-26 18:53:46 -0400

    add names for radio buttons, file input, textarea

--- frontend/document-summarizer/src/components/TextUploadForm.js
+++ frontend/document-summarizer/src/components/TextUploadForm.js
@@ -2,12 +2,24 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Form } from 'react-bootstrap'
 
+// TODO: download/import react-radio-group
+
 export default function TextUploadForm() {
     return (
         <Row>
             <Form>
                 <Form.Group>
-                    <Form.Check type='radio' label='Upload File (.docx)' checked />
+                    <Form.Check type='radio' name='inputType' label='Upload File (.docx)' checked />
+                </Form.Group>
+                <Form.Group>
+                    <Form.File id='fileInput' name='file'></Form.File>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Check type='radio' name='inputType' label='Copy and paste text' />
+                </Form.Group>
+                <Form.Group hidden>
+                    <Form.Label>Enter Document Text</Form.Label>
+                    <Form.Control as="textarea" rows='18' />
                 </Form.Group>
             </Form>
         </Row>
