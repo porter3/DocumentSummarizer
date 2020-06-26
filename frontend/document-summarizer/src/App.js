2aa622c3b83ab18db7589c3e040954aaa6f8c3c1
porter3 <jakeporter310@gmail.com>
2020-06-26 18:52:14 -0400

    add useState for isLoading, text, fileIsSelected

--- frontend/document-summarizer/src/App.js
+++ frontend/document-summarizer/src/App.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState, setState } from 'react'
 import UploadSection from './components/UploadSection'
 import TextUploadForm from './components/TextUploadForm'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/app.css'
 import { Container } from 'react-bootstrap'
 
 function App() {
+
+  const [ isLoading, setIsLoading ] = useState(false)
+  const [ text, setText ] = useState('')
+  const [ fileIsSelected, setFileIsSelected ] = useState(false)
+
   return (
     <Container className='app'>
       <UploadSection />
       <TextUploadForm />
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
