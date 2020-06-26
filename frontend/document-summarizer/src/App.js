f98327d80d9e8ef634d534e8407cc1697a0a224b
porter3 <jakeporter310@gmail.com>
2020-06-26 16:29:37 -0400

    add imports, bootstrap container component, uploadsection, textuploadform

--- frontend/document-summarizer/src/App.js
+++ frontend/document-summarizer/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from 'react'
+import UploadSection from './components/UploadSection'
+import TextUploadForm from './components/TextUploadForm'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/app.css'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container className='app'>
+      <UploadSection />
+      <TextUploadForm />
+    </Container>
   );
 }
 
