d64a80e4be475f1bebc8d3afd4d7680d0c7e5767
porter3 <jakeporter310@gmail.com>
2020-06-29 16:25:20 -0400

    remove button from component

--- frontend/document-summarizer/src/components/UploadSection.js
+++ frontend/document-summarizer/src/components/UploadSection.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col, Button } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 export default function UploadSection() {
     return (
-        <>
+        <div className='uploadSection'>
             <Row>
-                <Col xs={4}>
+                <Col>
                     <h3>Upload a file or enter text to summarize</h3>
                 </Col>
-                <Col>
-                    <Button>This is a button</Button>
-                </Col>
             </Row>
-        </>
+        </div>
     )
 }
