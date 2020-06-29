a5413a4744e7ab164c1ecde856646bd9825ac94f
porter3 <jakeporter310@gmail.com>
2020-06-29 16:24:49 -0400

    add cols and button component that was removed from UploadSection

--- frontend/document-summarizer/src/components/TextUploadForm.js
+++ frontend/document-summarizer/src/components/TextUploadForm.js
@@ -1,23 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import { FormControl, FormControlLabel, RadioGroup, Radio } from '@material-ui/core'
 
 
-export default function TextUploadForm({ handleFileChange, handleTextChange }) {
+export default function TextUploadForm({ uploadChoice, handleRadioChange, handleFileChange, handleTextChange, text }) {
+
+
     return (
         <Row>
-            <FormControl component="fieldset">
-                <RadioGroup aria-label="Upload Choices" name="uploadChoices" >
-                    <FormControlLabel value="uploadFile" control={<Radio />} label="Upload File" />
-                    <FormControlLabel value="enterText" control={<Radio />} label="Enter Text" />
-                </RadioGroup>
-                <div id="fileInput">
-                    <label htmlFor="file">Upload file:</label><br />
-                    <input type="file" name="file" id="file" onChange={handleFileChange} />
-                </div>
-                <textarea rows="18" cols="50" id="textEntry" onChange={handleTextChange} />
-            </FormControl>
+            <Col sm={6}>
+                <FormControl component="fieldset">
+                    <RadioGroup aria-label="Upload Choices" name="uploadChoices" onChange={e => handleRadioChange(e)}>
+                        <FormControlLabel value="fileUpload" control={<Radio />} label="Upload File" />
+                        <FormControlLabel value="text" control={<Radio />} label="Enter Text" />
+                    </RadioGroup>
+                    {uploadChoice === 'fileUpload' &&
+                        <div>
+                            <label htmlFor ="file">Upload file:</label><br />
+                            <input type="file" name="file" id="file" onChange={handleFileChange} />
+                        </div>
+                    }
+                    {uploadChoice === 'text' &&
+                        <textarea rows="18" cols="50" id="textEntry" onChange={handleTextChange} value={text} />
+                    }
+                </FormControl>
+            </Col>
+            <Col>
+                <Button>Summarize</Button>
+            </Col>
         </Row>
     )
 }
