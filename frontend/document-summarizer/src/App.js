62a5568547d02b61bcfdf21f6a7b3e67e47704be
porter3 <jakeporter310@gmail.com>
2020-06-30 16:42:20 -0400

    add uploadChoice and summary state/corresponding functions to set them, getSummary function using fetch/fetchSummary dependency. change UploadSection to UploadHeader.

--- frontend/document-summarizer/src/App.js
+++ frontend/document-summarizer/src/App.js
@@ -1,24 +1,65 @@
-import React, { useState, setState } from 'react'
-import UploadSection from './components/UploadSection'
+import React, { useState } from 'react'
+import UploadHeader from './components/UploadHeader'
 import TextUploadForm from './components/TextUploadForm'
+import { serverUrl } from './serverUrl'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/app.css'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+
 
 function App() {
 
   const [ isLoading, setIsLoading ] = useState(false)
+  const [ uploadChoice, setUploadChoice ] = useState('')
   const [ text, setText ] = useState('')
   const [ fileIsLoaded, setFileIsLoaded ] = useState(false)
+  const [ summary, setSummary ] = useState('')
+  
+  const handleUploadChoice = e => {
+    setUploadChoice(e.target.value)
+  }
 
   const handleTextChange = e => {
     setText(e.target.value)
   }
 
+  const fetchSummary = (url, body) => {
+    return fetch(url, {
+      method: 'POST',
+      body: body
+    })
+      .then(response => response.text())
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  const getSummary = async () => {
+    setIsLoading(true)
+    const url = serverUrl + '/uploadFile'
+    const form = new FormData()
+    form.append('file', document.getElementById('file').files[0])
+    const fetchedSummary = await fetchSummary(url, form)
+    setIsLoading(false)
+    setSummary(fetchedSummary)
+  }
+
   return (
     <Container className='app'>
-      <UploadSection />
-      <TextUploadForm handleTextChange={e => handleTextChange(e)} handleFileChange={() => setFileIsLoaded(!fileIsLoaded)} />
+      <Row>
+        <Col sm={6}>
+          <UploadHeader />
+          <TextUploadForm
+            uploadChoice={uploadChoice}
+            text={text}
+            handleRadioChange={e => handleUploadChoice(e)}
+            handleTextChange={e => handleTextChange(e)}
+            handleFileChange={() => setFileIsLoaded(!fileIsLoaded)}
+            handleClick={() => getSummary()}
+            isLoading={isLoading}
+          />
+        </Col>
+      </Row>
     </Container>
   )
 }
