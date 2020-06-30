55b3f3f7b8b3aa553cd94788e99b89f6c68e08ab
porter3 <jakeporter310@gmail.com>
2020-06-30 16:45:45 -0400

    rename UploadSection to UploadHeader

--- frontend/document-summarizer/src/components/UploadHeader.js
+++ frontend/document-summarizer/src/components/UploadHeader.js
@@ -2,9 +2,9 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Col } from 'react-bootstrap'
 
-export default function UploadSection() {
+export default function UploadHeader() {
     return (
-        <div className='uploadSection'>
+        <div className='uploadHeader'>
             <Row>
                 <Col>
                     <h3>Upload a file or enter text to summarize</h3>
