53dd62ebc6c4733fdc1a2cc64fe087668840eb8b
porter3 <jakeporter310@gmail.com>
2020-07-02 13:04:40 -0400

    add request mapping for controller to '/summarize', change mapping for '/uploadFile' to '/file', change 'uploadFile() to summarizeFile()', add '/text' endpoint and corresponding method for handling requests with pure text

--- src/main/java/com/jakeporter/DocumentAnalyzer/controllers/FileController.java
+++ src/main/java/com/jakeporter/DocumentAnalyzer/controllers/FileController.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/summarize")
 public class FileController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -25,20 +26,34 @@ public class FileController {
     @Autowired
     FileService fileService;
 
-    @PostMapping("/uploadFile")
-    public ResponseEntity<Map<String, String>> uploadFile(@RequestParam("file") MultipartFile file) {
+    @PostMapping("/file")
+    public ResponseEntity<Map<String, String>> summarizeFile(@RequestParam("file") MultipartFile file) {
         String summary;
         Map<String, String> response = new HashMap();
 
-        try{
+        try {
             fileService.uploadFile(file);
         } catch (FileStorageException e) {
             response.put(ERROR_KEY, e.getMessage());
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        try{
+        try {
             summary = fileService.summarize(file);
             logger.info("Summary: " + summary);
+        } catch (IOException e) {
+            response.put(ERROR_KEY, e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put(SUCCESS_KEY, summary);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/text")
+    public ResponseEntity<Map<String, String>> summarizeText(@RequestBody String text) {
+        String summary;
+        Map<String, String> response = new HashMap();
+        try {
+            summary = fileService.summarize(text);
         } catch (IOException e) {
             response.put(ERROR_KEY, e.getMessage());
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
