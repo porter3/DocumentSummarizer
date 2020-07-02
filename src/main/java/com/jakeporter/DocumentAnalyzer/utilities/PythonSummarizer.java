ff26d20e9e8ae92a19b472ded041884897ba7b6d
porter3 <jakeporter310@gmail.com>
2020-07-02 13:09:45 -0400

    remove lines that have been put in parent class's readResult() method and use readResult()

--- src/main/java/com/jakeporter/DocumentAnalyzer/utilities/PythonSummarizer.java
+++ src/main/java/com/jakeporter/DocumentAnalyzer/utilities/PythonSummarizer.java
@@ -1,21 +1,9 @@
 package com.jakeporter.DocumentAnalyzer.utilities;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+
 
 public class PythonSummarizer extends DocumentSummarizer {
 
@@ -35,11 +23,8 @@ public class PythonSummarizer extends DocumentSummarizer {
         ProcessBuilder processBuilder = new ProcessBuilder("python", filePath, text);
         processBuilder.redirectErrorStream(true);
         Process process = processBuilder.start();
-
-        // InputStreamReader reads bytes and decodes them into characters
-        BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String result = in.readLine();
-        logger.info("Output result: " + result);
-        return result;
+        return readResult(process);
     }
+
+
 }
