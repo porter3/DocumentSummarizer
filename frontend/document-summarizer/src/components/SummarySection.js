e452a7b459b90b75973d5be67fe3e95ce6f1de50
porter3 <jakeporter310@gmail.com>
2020-07-02 12:34:18 -0400

    add div and id around whole component, pass in 'isLoading' prop, add ids for each motion.div, add bootstrap Alert component for error messages, add Loader component/conditional rendering

--- frontend/document-summarizer/src/components/SummarySection.js
+++ frontend/document-summarizer/src/components/SummarySection.js
@@ -1,27 +1,35 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Loader from './Loader'
 import { motion } from 'framer-motion'
+import { Alert } from 'react-bootstrap'
 
-export default function SummarySection({ summary, error }) {
+export default function SummarySection({ summary, error, isLoading }) {
 
     const summaryAnimation = { opacity: summary ? 1 : 0 }
     const errorAnimation = { opacity: error ? 1 : 0 }
 
     return (
-        <>
+        <div id='summarySection'>
             <motion.div
-                className="summary"
+                id='summary'
                 animate={summaryAnimation}>
                 {summary &&
                     `${summary}`
                 }
             </motion.div>
             <motion.div
-                className="errorMsg"
+                id='error'
                 animate={errorAnimation}>
-                {error &&
-                    `${error}`
-                }
+                <Alert variant='danger'>
+                    {error &&
+                        `${error}`
+                    }
+                </Alert>
             </motion.div>
-        </>
+            {isLoading &&
+                <Loader />
+            }
+        </div>
     )
 }
