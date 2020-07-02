442b94c09390bab750b2f337d44bcfcd4962244e
porter3 <jakeporter310@gmail.com>
2020-07-02 13:59:25 -0400

    change script String to final, add constructors (default and one that accepts a FileTextExtractor)

--- src/main/java/com/jakeporter/DocumentAnalyzer/utilities/summarizers/PythonSummarizer.java
+++ src/main/java/com/jakeporter/DocumentAnalyzer/utilities/summarizers/PythonSummarizer.java
@@ -1,5 +1,6 @@
-package com.jakeporter.DocumentAnalyzer.utilities;
+package com.jakeporter.DocumentAnalyzer.utilities.summarizers;
 
+import com.jakeporter.DocumentAnalyzer.utilities.textExtractors.FileTextExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
@@ -7,9 +8,18 @@ import java.io.IOException;
 
 public class PythonSummarizer extends DocumentSummarizer {
 
+    private final String SCRIPT = "textSummarizer.py";
+
+    public PythonSummarizer(){
+        super();
+    }
+
+    public PythonSummarizer(FileTextExtractor extractor) {
+        super(extractor);
+    }
+
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private String script = "textSummarizer.py";
 
     @Override
     protected String computeSummary(String text) throws IOException {
@@ -18,7 +28,7 @@ public class PythonSummarizer extends DocumentSummarizer {
 
     private String runPythonScript(String text) throws IOException {
         // filePath starts with "/C:/..." - the first forward slash needs to go for it to work
-        String filePath = this.getClass().getClassLoader().getResource(script).getPath().substring(1);
+        String filePath = this.getClass().getClassLoader().getResource(SCRIPT).getPath().substring(1);
         logger.info(filePath);
         ProcessBuilder processBuilder = new ProcessBuilder("python", filePath, text);
         processBuilder.redirectErrorStream(true);
