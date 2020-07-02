fc810d16503d5b0416739dd8650d4e0734a4331b
porter3 <jakeporter310@gmail.com>
2020-07-02 17:04:07 -0400

    add HttpMessageNotReadableException/MissingServletRequestPartException/UnsupportedFileFormatException handlers

--- src/main/java/com/jakeporter/DocumentAnalyzer/controllers/ExceptionHandlerController.java
+++ src/main/java/com/jakeporter/DocumentAnalyzer/controllers/ExceptionHandlerController.java
@@ -3,21 +3,37 @@ package com.jakeporter.DocumentAnalyzer.controllers;
 import com.jakeporter.DocumentAnalyzer.error.Error;
 import com.jakeporter.DocumentAnalyzer.exceptions.ProblematicTextException;
 import com.jakeporter.DocumentAnalyzer.exceptions.TextTooShortException;
+import com.jakeporter.DocumentAnalyzer.exceptions.UnsupportedFileFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 
 @ControllerAdvice
 @RestController
 public class ExceptionHandlerController {
 
+    // thrown by Spring
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<Error> handleFileSizeLimitExceeded(MaxUploadSizeExceededException e) {
         return new ResponseEntity<>(new Error("File size cannot exceed 1MB."), HttpStatus.PAYLOAD_TOO_LARGE);
+    }
+
+    // thrown by Spring first for /file endpoint and is the only exception thrown for /text endpoint
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Error> handleNoBody(HttpMessageNotReadableException e) {
+        return new ResponseEntity<>(new Error("You must upload a document or text to generate a summary."), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    // thrown by Spring after HttpMessageNotReadableException via /file endpoint
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    public ResponseEntity<Error> handleNoRequestPart(MissingServletRequestPartException e) {
+        return new ResponseEntity<>(new Error("You must upload a document or text to generate a summary."), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler(TextTooShortException.class)
@@ -28,6 +44,11 @@ public class ExceptionHandlerController {
     // for more generic text-based errors that cause issues in the Python script
     @ExceptionHandler(ProblematicTextException.class)
     public ResponseEntity<Error> handleProblematicText(ProblematicTextException e) {
+        return new ResponseEntity<>(new Error(e.getMessage()), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(UnsupportedFileFormatException.class)
+    public ResponseEntity<Error> handleUnsupportedFileFormat(UnsupportedFileFormatException e) {
         return new ResponseEntity<>(new Error(e.getMessage()), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
