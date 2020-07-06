1dbe2bf0ccf32d078a58899757450d2646c3188f
porter3 <jakeporter310@gmail.com>
2020-07-06 13:22:37 -0400

    modify computeSummary() and runPythonScript() to accept String arrays, modify runPythonScript() to construct ProcessBuilder from the 'python'/path/String[] commands, add makeProcessArgs()

--- src/main/java/com/jakeporter/DocumentAnalyzer/utilities/summarizers/PythonSummarizer.java
+++ src/main/java/com/jakeporter/DocumentAnalyzer/utilities/summarizers/PythonSummarizer.java
@@ -4,15 +4,14 @@ import com.jakeporter.DocumentAnalyzer.utilities.textExtractors.FileTextExtracto
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class PythonSummarizer extends DocumentSummarizer {
 
     private final String SCRIPT = "textSummarizer.py";
 
-    public PythonSummarizer(){
-        super();
-    }
+    public PythonSummarizer(){ super(); }
 
     public PythonSummarizer(FileTextExtractor extractor) {
         super(extractor);
@@ -22,19 +21,36 @@ public class PythonSummarizer extends DocumentSummarizer {
 
 
     @Override
-    protected String computeSummary(String text) throws IOException {
-        return runPythonScript(text);
+    protected String computeSummary(String[] textChunks) throws IOException {
+        return runPythonScript(textChunks);
     }
 
-    private String runPythonScript(String text) throws IOException {
+    private String runPythonScript(String[] textChunks) throws IOException {
         // filePath starts with "/C:/..." - the first forward slash needs to go for it to work
         String filePath = this.getClass().getClassLoader().getResource(SCRIPT).getPath().substring(1);
         logger.info("Script path: " + filePath);
-        ProcessBuilder processBuilder = new ProcessBuilder("python", filePath, text);
+        String[] processArgs = makeProcessArgs(textChunks, "python", filePath);
+        ProcessBuilder processBuilder = new ProcessBuilder(processArgs);
         processBuilder.redirectErrorStream(true);
         Process process = processBuilder.start();
         return readResult(process);
     }
 
+    private String[] makeProcessArgs(String[] textChunks, String... argStrings) {
+        String[] processArgs = new String[argStrings.length + textChunks.length];
+        // argument strings that aren't in an array will be first
+        int argsIndex = 0;
+        while (argsIndex < processArgs.length) {
+            for (int i = 0; i < argStrings.length; i++) {
+                processArgs[argsIndex] = argStrings[i];
+                argsIndex++;
+            }
+            for (int i = 0; i < textChunks.length; i++) {
+                processArgs[argsIndex] = textChunks[i];
+                argsIndex++;
+            }
+        }
+        return processArgs;
+    }
 
 }
