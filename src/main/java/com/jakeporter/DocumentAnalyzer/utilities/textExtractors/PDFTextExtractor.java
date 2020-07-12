72171fb3beffa76726fd78bee211bc0eb59b432f
porter3 <jakeporter310@gmail.com>
2020-07-12 16:36:04 -0400

    remove unused import

--- src/main/java/com/jakeporter/DocumentAnalyzer/utilities/textExtractors/PDFTextExtractor.java
+++ src/main/java/com/jakeporter/DocumentAnalyzer/utilities/textExtractors/PDFTextExtractor.java
@@ -9,7 +9,6 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class PDFTextExtractor implements FileTextExtractor {
 
