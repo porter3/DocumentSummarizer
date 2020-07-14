2901ccc76d753a34f556778bc85f8e51311a6533
porter3 <jakeporter310@gmail.com>
2020-07-14 17:16:51 -0400

    remove throws IOException clause

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/textExtractors/FileTextExtractor.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/textExtractors/FileTextExtractor.java
@@ -6,5 +6,5 @@ import java.io.IOException;
 
 public interface FileTextExtractor {
 
-    public String extractText(MultipartFile file) throws IOException;
+    public String extractText(MultipartFile file);
 }
