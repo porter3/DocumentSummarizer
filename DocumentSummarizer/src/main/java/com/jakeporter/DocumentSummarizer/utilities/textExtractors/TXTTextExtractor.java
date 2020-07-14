18ab070b87e9363ff8f0e69246fa90a61cec48b9
porter3 <jakeporter310@gmail.com>
2020-07-14 17:17:38 -0400

    handle all IOExceptions in extractText() and remove throws clause

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/textExtractors/TXTTextExtractor.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/textExtractors/TXTTextExtractor.java
@@ -12,10 +12,11 @@ public class TXTTextExtractor implements FileTextExtractor {
     private static final String TXT_EXCEPTION_MESSAGE = "Something went wrong processing the text file.";
 
     @Override
-    public String extractText(MultipartFile file) throws IOException {
+    public String extractText(MultipartFile file) {
         StringBuilder builder = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        BufferedReader reader = null;
         try {
+            reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
             String line = reader.readLine();
             while (line != null) {
                 builder.append(line);
@@ -23,7 +24,11 @@ public class TXTTextExtractor implements FileTextExtractor {
             }
             reader.close();
         } catch (IOException e) {
-            reader.close();
+            try{
+                reader.close();
+            } catch (IOException f) {
+                throw new TXTIssueException(TXT_EXCEPTION_MESSAGE);
+            }
             throw new TXTIssueException(TXT_EXCEPTION_MESSAGE);
         }
         return builder.toString();
