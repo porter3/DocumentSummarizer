0b5559b0536722de66b1c474c6de329092e41e55
porter3 <jakeporter310@gmail.com>
2020-07-20 12:31:43 -0400

    add constants for choosing between local and AWS builds, add nested FileUploader and FileRemover

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/service/FileService.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/service/FileService.java
@@ -3,6 +3,8 @@ package com.jakeporter.DocumentSummarizer.service;
 import com.jakeporter.DocumentSummarizer.exceptions.FileDeletionException;
 import com.jakeporter.DocumentSummarizer.exceptions.GenericFileException;
 import com.jakeporter.DocumentSummarizer.utilities.fileUtils.FileInfoGetter;
+import com.jakeporter.DocumentSummarizer.utilities.fileUtils.removers.FileRemover;
+import com.jakeporter.DocumentSummarizer.utilities.fileUtils.removers.LocalRemover;
 import com.jakeporter.DocumentSummarizer.utilities.fileUtils.uploaders.AWSS3Uploader;
 import com.jakeporter.DocumentSummarizer.utilities.fileUtils.uploaders.FileUploader;
 import com.jakeporter.DocumentSummarizer.utilities.summarizers.DocumentSummarizer;
@@ -17,21 +19,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Set;
 
 @Service
 public class FileService {
 
-    @Autowired
-    @Qualifier("localUploader")
-    private FileUploader uploader;
+    private final static boolean IS_LOCAL = false;
+    private final static String BUILD = IS_LOCAL ? "local" : "aws";
 
     @Value("${upload.dir:${user.home}}")
     private String UPLOAD_DIRECTORY;
+
+    @Autowired
+    @Qualifier(BUILD)
+    private FileUploader uploader;
+
+    @Autowired
+    @Qualifier(BUILD)
+    private FileRemover remover;
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -41,7 +46,7 @@ public class FileService {
 
     public Set<String> summarize(MultipartFile file) {
         FileInfoGetter infoGetter = new FileInfoGetter();
-        if (uploader instanceof AWSS3Uploader) {
+        if (!IS_LOCAL) {
             file = getFileFromS3(file);
         }
         FileType fileType = infoGetter.getFileType(FilenameUtils.getExtension(file.getOriginalFilename()));
@@ -54,7 +59,7 @@ public class FileService {
         } catch (Exception e) { // Don't know what uncaught exceptions could throw this at the moment, but I want to ensure the deletion of any uploaded files
             throw new GenericFileException("Something went wrong.");
         } finally {
-            deleteFile(infoGetter.getFileLocation(file, UPLOAD_DIRECTORY));
+            remover.deleteFile(infoGetter.getFileLocation(file, UPLOAD_DIRECTORY));
         }
         return summaries;
     }
@@ -69,12 +74,4 @@ public class FileService {
         return null;
     }
 
-    private void deleteFile(Path filePath) {
-        try{
-            Files.delete(filePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new FileDeletionException("Error deleting file from " + filePath.toString() + ".");
-        }
-    }
 }
