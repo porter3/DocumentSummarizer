dff2655262a38a5723dd0dbff2d2a92f81df24f4
porter3 <jakeporter310@gmail.com>
2020-07-23 12:37:47 -0400

    delete

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/fileUtils/uploaders/FileUploader.java
+++
@@ -1,8 +0,0 @@
-package com.jakeporter.DocumentSummarizer.utilities.fileUtils.uploaders;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUploader {
-
-    public String uploadFile(MultipartFile file);
-}
