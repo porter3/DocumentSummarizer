b7ad120859aa2dec272e0f2a16409ba65853cb2f
porter3 <jakeporter310@gmail.com>
2020-07-26 14:29:31 -0400

    factor handleResultIssues() out of runPythonScript() and into computeSummary(), factor out I/O operations to new JarScriptRunner class

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/summarizers/PythonSummarizer.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/summarizers/PythonSummarizer.java
@@ -1,19 +1,19 @@
 package com.jakeporter.DocumentSummarizer.utilities.summarizers;
 
 import com.jakeporter.DocumentSummarizer.exceptions.*;
+import com.jakeporter.DocumentSummarizer.utilities.scriptRunners.JARScriptRunner;
 import com.jakeporter.DocumentSummarizer.utilities.textExtractors.FileTextExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
 import java.util.*;
 
 
 public class PythonSummarizer extends DocumentSummarizer {
 
+    private static final boolean IS_PRODUCTION_BUILD = true;
+    private static final String PYTHON_CMD = IS_PRODUCTION_BUILD ? "python3" : "python";
     private static final String SCRIPT = "textSummarizer.py";
-    // filePath starts with "/C:/..." - the first forward slash needs to go (.substring(1)) for it to work on Windows (unsure about other OSs)
-    private static final String FILE_PATH = PythonSummarizer.class.getClassLoader().getResource(SCRIPT).getPath().substring(1);
     private static final String SUMMARY_DELIMITER = ":::";
 
     public PythonSummarizer(){ super(); }
@@ -27,76 +27,26 @@ public class PythonSummarizer extends DocumentSummarizer {
     @Override
     protected Set<String> computeSummaries(String text) {
         String summariesString = runPythonScript(text);
+        handleResultIssues(summariesString);
         return new LinkedHashSet<>(Arrays.asList(summariesString.split(SUMMARY_DELIMITER)));
     }
 
     private String runPythonScript(String text)  {
-        Process process;
-        String result = "";
-        logger.info("Script path: " + FILE_PATH);
-        ProcessBuilder processBuilder = new ProcessBuilder("python", FILE_PATH, SUMMARY_DELIMITER);
-        processBuilder.redirectErrorStream(true);
-        try {
-            process = processBuilder.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new PythonScriptException("Something went wrong processing the file.");
-        }
-        writeToStdIn(process, text);
-        result = getResultFromStdOut(process);
-        handleResultIssues(result);
+        JARScriptRunner scriptRunner = new JARScriptRunner(PYTHON_CMD, SCRIPT);
+        String result = scriptRunner.runPythonScript(text, SUMMARY_DELIMITER);
         return result;
     }
 
-    private void writeToStdIn(Process process, String text) {
-        OutputStream pythonStdIn = process.getOutputStream();
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(pythonStdIn));
-        try {
-            writer.write(text);
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new WriterException("Something went wrong in getting the summary.");
-        }
-    }
-
-    private String getResultFromStdOut(Process process) {
-        final String READING_EXCEPTION_MESSAGE = "Something went wrong in getting the summary.";
-        StringBuilder builder = new StringBuilder();
-        InputStream stdOut = process.getInputStream();
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new InputStreamReader(stdOut));
-            String line = reader.readLine();
-            while (line != null) {
-                builder.append(line);
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            try {
-                reader.close();
-            } catch (IOException f) {
-                f.printStackTrace();
-                throw new ResultReadingException(READING_EXCEPTION_MESSAGE);
-            }
-            e.printStackTrace();
-            throw new ResultReadingException(READING_EXCEPTION_MESSAGE);
-        }
-        return builder.toString();
-    }
-
-    private void handleResultIssues(String result) {
+    private void handleResultIssues(String scriptOutput) {
         final String pyCompilerErrorLine = "Traceback (most recent call last):";
         final String genericErrorLine = "Something went wrong with executing the Python script.";
-        if (result.isBlank()) {
+        if (scriptOutput.isBlank()) {
             throw new TextTooShortException("The text you tried to summarize is either too short or too repetitive to do so.");
         }
-        if (result.equals(pyCompilerErrorLine)) {
+        if (scriptOutput.equals(pyCompilerErrorLine)) {
             throw new ProblematicTextException("The text you tried to summarize doesn't summarize well.");
         }
-        if (result.equals(genericErrorLine)) {
+        if (scriptOutput.equals(genericErrorLine)) {
             throw new PythonScriptException("Something went wrong on our end.");
         }
     }
