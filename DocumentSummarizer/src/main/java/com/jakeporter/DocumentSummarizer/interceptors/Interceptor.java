aaf9f0c135a918ff97bec62cfe5adec824024bf1
porter3 <jakeporter310@gmail.com>
2020-07-28 17:09:28 -0400

    rename to Interceptor

---
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/interceptors/Interceptor.java
@@ -0,0 +1,20 @@
+package com.jakeporter.DocumentSummarizer.interceptors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Interceptor extends HandlerInterceptorAdapter {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info(request.getMethod() + ": " + request.getRequestURI());
+        return true;
+    }
+}
