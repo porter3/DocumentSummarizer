a7e3dc96a72a4d4591855e3b923748c4ba49b7ec
porter3 <jakeporter310@gmail.com>
2020-07-28 16:52:32 -0400

    factor cors configuration out of main class and into separate config class, add addInterceptors() to configuration

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/App.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/App.java
@@ -1,30 +1,11 @@
 package com.jakeporter.DocumentSummarizer;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class App {
-
-    @Value("${frontend.url}")
-    String frontendUrl;
-
-    @Value("${my.ip}")
-    String myIp;
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins(frontendUrl, myIp, "http://localhost:3000");
-            }
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
---
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/Config.java
@@ -0,0 +1,30 @@
+package com.jakeporter.DocumentSummarizer;
+
+import com.jakeporter.DocumentSummarizer.interceptors.FileInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    String frontendUrl;
+
+    @Value("${my.ip}")
+    String myIp;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins(frontendUrl, myIp, "http://localhost:3000");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new FileInterceptor());
+    }
+}
