5bdcdb4250690ffda17484dfe7e147afb2466d68
porter3 <jakeporter310@gmail.com>
2020-07-29 16:02:14 -0400

    rename FileInterceptor to Interceptor

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/Config.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/Config.java
@@ -1,10 +1,8 @@
 package com.jakeporter.DocumentSummarizer;
 
-import com.jakeporter.DocumentSummarizer.interceptors.FileInterceptor;
+import com.jakeporter.DocumentSummarizer.interceptors.Interceptor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,6 +23,6 @@ public class Config implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new FileInterceptor());
+        registry.addInterceptor(new Interceptor());
     }
 }
