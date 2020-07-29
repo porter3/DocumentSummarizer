aa17fd3614825a1ea00a7ae2599c01b0515694f3
porter3 <jakeporter310@gmail.com>
2020-07-29 18:06:59 -0400

    refactor

--- frontend/document-summarizer/src/serverUrl.js
+++ frontend/document-summarizer/src/serverUrl.js
@@ -1,3 +1,4 @@
 const localBuild = true
 const path = '/summarize'
-export const serverUrl = localBuild ? 'http://localhost:5000' + path : 'http://documentsummarizer-env.eba-5pbvjawt.us-east-1.elasticbeanstalk.com' + path
+const serverUrl = localBuild ? 'http://localhost:5000' : 'http://documentsummarizer-env.eba-5pbvjawt.us-east-1.elasticbeanstalk.com'
+export default serverUrl + path
