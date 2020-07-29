a9810052541afae24b3b64454f733e34474480ed
porter3 <jakeporter310@gmail.com>
2020-07-29 16:06:17 -0400

    remove classes

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/fileUtils/removers/FileRemover.java
+++
@@ -1,8 +0,0 @@
-package com.jakeporter.DocumentSummarizer.utilities.fileUtils.removers;
-
-import java.nio.file.Path;
-
-public interface FileRemover {
-
-    public void deleteFile(Path filePath);
-}
