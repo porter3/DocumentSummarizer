66448d8c37ba879cf2a2b4720576ea6549538f69
porter3 <jakeporter310@gmail.com>
2020-07-30 15:55:29 -0400

    rename error constants, add NO_VALID_WORDS_ERROR to correspond with a new exception being added to script

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/summarizers/PythonSummarizer.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/summarizers/PythonSummarizer.java
@@ -40,19 +40,20 @@ public class PythonSummarizer extends DocumentSummarizer {
     private void handleResultIssues(String scriptOutput) {
         final String UTF8_VALIDATION_ERROR = "Non-UTF-8 code starting with";
         final String ENCHANT_ERROR = "enchant_dict_check: assertion 'g_utf8_validate(word, len, NULL)";
-        final String PY_COMPILER_ERROR_LINE = "Traceback (most recent call last):";
-        final String GENERIC_ERROR_LINE = "Something went wrong with executing the Python script.";
+        final String NO_VALID_WORDS_ERROR = "NO_VALID_WORDS_ERROR";
+        final String PY_COMPILER_ERROR = "Traceback (most recent call last):";
+        final String GENERIC_ERROR = "GENERIC_ERROR";
         if (scriptOutput.isBlank()) {
             throw new SummaryException("The text you tried to summarize is either too short or too repetitive to do so.");
         }
-        if (scriptOutput.contains(ENCHANT_ERROR) || scriptOutput.contains(UTF8_VALIDATION_ERROR)) {
+        if (scriptOutput.contains(ENCHANT_ERROR) || scriptOutput.contains(UTF8_VALIDATION_ERROR) || scriptOutput.equals(NO_VALID_WORDS_ERROR)) {
             throw new SummaryException("There was a problem with your text. Ensure all characters in your text are UTF-8. " +
                     "If you uploaded a file, ensure it has the correct extension.");
         }
-        if (scriptOutput.equals(PY_COMPILER_ERROR_LINE)) {
+        if (scriptOutput.equals(PY_COMPILER_ERROR)) {
             throw new SummaryException("The text you tried to summarize doesn't summarize well.");
         }
-        if (scriptOutput.equals(GENERIC_ERROR_LINE)) {
+        if (scriptOutput.equals(GENERIC_ERROR)) {
             throw new SummaryException("Something went wrong on our end.");
         }
     }
