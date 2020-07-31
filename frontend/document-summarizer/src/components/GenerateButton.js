5ee39385f8fe898b0fa9f468a7dc5c29e9c97106
porter3 <jakeporter310@gmail.com>
2020-07-31 17:16:03 -0400

    import Button from material-ui instead of bootstrap

--- frontend/document-summarizer/src/components/GenerateButton.js
+++ frontend/document-summarizer/src/components/GenerateButton.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import Button from '@material-ui/core/Button'
 
 export default function GenerateButton({ isLoading, isBadExtension, handleClick }) {
+
     return (
-        <Button
-            id='generateButton'
-            variant='primary'
-            disabled={isLoading || isBadExtension}
-            size='lg'
-            onClick={handleClick}>
-            {isLoading ? 'Generating summary...' : 'Generate Summary'}
-        </Button>
+            <Button
+                id='generateButton'
+                variant='contained'
+                color='primary'
+                disabled={isLoading || isBadExtension}
+                size='large'
+                onClick={handleClick}>
+                {isLoading ? 'Generating summary...' : 'Generate Summary'}
+            </Button>
     )
 }
