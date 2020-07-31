78a7ff38aa4beed58eecb0748bc0948c0c6c42c3
porter3 <jakeporter310@gmail.com>
2020-07-31 17:16:44 -0400

    import Paper from full path instead of parent directory

--- frontend/document-summarizer/src/components/SummarySection.js
+++ frontend/document-summarizer/src/components/SummarySection.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Paper } from '@material-ui/core'
+import Paper from '@material-ui/core/Paper'
 import Loader from './Loader'
 
 
