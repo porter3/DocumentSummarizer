7eb1bf04685525b72b2ecc4fc2b2e927c3c6c0bf
porter3 <jakeporter310@gmail.com>
2020-08-02 20:19:23 -0400

    add file size validation

--- frontend/document-summarizer/src/components/GenerateButton.js
+++ frontend/document-summarizer/src/components/GenerateButton.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-export default function GenerateButton({ isLoading, isBadExtension, handleClick }) {
+export default function GenerateButton({ isLoading, isBadExtension, isTooLargeFile, handleClick }) {
 
     return (
             <Button
                 id='generateButton'
                 variant='contained'
                 color='primary'
-                disabled={isLoading || isBadExtension}
+                disabled={isLoading || isBadExtension || isTooLargeFile}
                 size='large'
                 onClick={handleClick}>
                 {isLoading ? 'Generating summary...' : 'Generate Summary'}
