c0aa0361b77cb607bcdd983878c8d1b62fe1b150
porter3 <jakeporter310@gmail.com>
2020-08-02 20:19:40 -0400

    wrap header in Typogaphy

--- frontend/document-summarizer/src/components/UploadHeader.js
+++ frontend/document-summarizer/src/components/UploadHeader.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Col } from 'react-bootstrap'
+import Typography from '@material-ui/core/Typography'
 
 export default function UploadHeader() {
     return (
         <div id='uploadHeader'>
             <Row>
                 <Col>
-                    <h3>Upload a file or enter text to summarize</h3>
+                    <Typography variant='h3'><h3>Upload a file or enter text to summarize</h3></Typography>
                 </Col>
             </Row>
         </div>
