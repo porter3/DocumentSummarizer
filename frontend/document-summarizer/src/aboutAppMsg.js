a7943d6c3978ea598951d14b72f859b5b5e02347
porter3 <jakeporter310@gmail.com>
2020-08-04 16:53:31 -0400

    add message string for 'About This App' popover

---
+++ frontend/document-summarizer/src/aboutAppMsg.js
@@ -0,0 +1,5 @@
+export default 'This app generates summaries via the Natural Language Toolkit for Python, ' +
+    'using existing sentences from the text. No new sentences are generated. The Python script determines ' + 
+    'sentence importance based on a variety of factors and outputs all sentences above a specific threshold. ' +
+    'If a text has enough sentences, multiple summaries will be generated with varying thresholds and a slider will ' +
+    'appear underneath the summary section to allow for selecting them.'
