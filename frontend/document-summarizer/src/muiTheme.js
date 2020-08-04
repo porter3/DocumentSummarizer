f5ad7997abd1d0c6451ddd10ed5953baba3ef587
porter3 <jakeporter310@gmail.com>
2020-08-04 16:59:18 -0400

    add 'isDarkModeEnabled' function arg and conditional for palette.type, add 'body2' variant as a typography key

--- frontend/document-summarizer/src/muiTheme.js
+++ frontend/document-summarizer/src/muiTheme.js
@@ -1,24 +1,29 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import './css/fonts/fontFaces.css'
 
-export default createMuiTheme({
-    palette: { 
-        type: 'light',
-        primary: {
-            main: '#6DD3CE',
-            dark: '#639FAB'
+export default function createTheme(isDarkModeEnabled) {
+    return createMuiTheme({
+        palette: { 
+            type: isDarkModeEnabled ? 'dark' : 'light',
+            primary: {
+                main: '#6DD3CE',
+                dark: '#639FAB'
+            },
+            secondary: {
+                main: '#52CBC5'
+            }
         },
-        secondary: {
-            main: '#52CBC5'
+        typography: {
+            fontFamily: [ 'montserratlight', 'Times New Roman' ].join(','),
+            body2: {
+                fontFamily: [ 'montserratmedium', 'Times New Roman' ].join(',')
+            },
+            h3: {
+                fontSize: '1.75rem'
+            },
+            button: {
+                fontFamily: [ 'montserratmedium', 'Times New Roman' ].join(',')
+            }
         }
-    },
-    typography: {
-        h3: {
-            fontSize: '1.75rem'
-        },
-        button: {
-            fontFamily: [ 'montserratmedium', 'Times New Roman' ].join(',')
-        },
-        fontFamily: [ 'montserratlight', 'Times New Roman' ].join(',')
-    }
-})
+    })
+}
