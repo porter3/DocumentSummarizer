fbad0a5c9df47e055670651226fc5d6f7215eade
porter3 <jakeporter310@gmail.com>
2020-08-04 17:09:41 -0400

    minor refactoring

--- frontend/document-summarizer/src/components/SummarySection.js
+++ frontend/document-summarizer/src/components/SummarySection.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Paper from '@material-ui/core/Paper'
 import Alert from '@material-ui/lab/Alert'
 import Loader from './Loader'
 
 
 export default function SummarySection({ summaries, errorMessage, isLoading, summaryChoice, loaderMessage }) {
-    // only display first element of summaries for now
     return (
         <div id='summarySection'>
             {summaries.length !== 0 &&
                 <Paper
-                id='paper'
-                elevation={3}>
+                    id='paper'
+                    elevation={3}>
                     <div id='paperText'>
                         {summaries[summaryChoice]}
                     </div>
--- frontend/document-summarizer/src/components/UploadHeader.js
+++ frontend/document-summarizer/src/components/UploadHeader.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Col } from 'react-bootstrap'
 import Typography from '@material-ui/core/Typography'
 
