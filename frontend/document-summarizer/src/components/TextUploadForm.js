85f61b0b97172d0366e1b9ef5b15efc7c10e54fd
porter3 <jakeporter310@gmail.com>
2020-08-04 17:09:25 -0400

    refactor, add startIcons to Upload and Clear buttons, add textareaStyle for applying background and text colors in dark mode

--- frontend/document-summarizer/src/components/TextUploadForm.js
+++ frontend/document-summarizer/src/components/TextUploadForm.js
@@ -1,12 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
-import { FormControl, FormControlLabel, RadioGroup, Radio, Typography, Button } from '@material-ui/core'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import FormControl from '@material-ui/core/FormControl'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Radio from '@material-ui/core/Radio'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import CloudUploadOutlinedIcon from '@material-ui/icons/CloudUploadOutlined'
+import ClearOutlinedIcon from '@material-ui/icons/ClearOutlined'
 import Alert from '@material-ui/lab/Alert'
 import AlertTitle from '@material-ui/lab/AlertTitle'
 
+export default function TextUploadForm({ theme, uploadChoice, text, fileExtension, isBadExtension, fileName, isTooLargeFile, isDarkModeEnabled, handleRadioChange, handleTextChange, handleFileChange, handleClick }) {
 
-export default function TextUploadForm({ uploadChoice, text, fileExtension, isBadExtension, fileName, isTooLargeFile, handleRadioChange, handleTextChange, handleFileChange, handleClick }) {
+    const textareaStyle = isDarkModeEnabled ? { backgroundColor: '#6B6B6B', color: '#ffffff' } : null
 
     return (
         <div>
@@ -14,13 +22,15 @@ export default function TextUploadForm({ uploadChoice, text, fileExtension, isBa
                 <Col xs={12}>
                     <FormControl component="fieldset">
                         <RadioGroup aria-label="Upload Choices" name="uploadChoices" onChange={e => handleRadioChange(e)}>
-                            <FormControlLabel value="fileUpload" control={<Radio />}
-                            label={
-                                <>
-                                    <Typography>Upload File</Typography>
-                                    <Typography><small>(.docx, .doc, .pdf, .txt)</small></Typography>
-                                </>
-                            }/>
+                            <FormControlLabel
+                                value="fileUpload"
+                                control={<Radio />}
+                                label={
+                                    <>
+                                        <Typography>Upload File</Typography>
+                                        <Typography><small>(.docx, .doc, .pdf, .txt)</small></Typography>
+                                    </>
+                                }/>
                             <FormControlLabel value="text" control={<Radio />}
                             label={
                                 <Typography>Enter Text</Typography>
@@ -34,7 +44,13 @@ export default function TextUploadForm({ uploadChoice, text, fileExtension, isBa
                     <div hidden={uploadChoice !== 'fileUpload'}>
                         <input type="file" name="file" id="file" style={{ display: 'none' }} onChange={handleFileChange} />
                         <label htmlFor='file'>
-                            <Button variant='outlined' component='span' color='secondary'>Upload File</Button>
+                            <Button
+                                variant='outlined'
+                                component='span'
+                                color='secondary'
+                                startIcon={<CloudUploadOutlinedIcon />}>
+                                    Upload File
+                            </Button>
                         </label>
                         <Row>
                             <Typography id='fileName'>
@@ -73,10 +89,18 @@ export default function TextUploadForm({ uploadChoice, text, fileExtension, isBa
                                     variant='outlined'
                                     component='span'
                                     color='secondary'
+                                    startIcon={<ClearOutlinedIcon />}
                                     onClick={handleClick}>
-                                        Clear
+                                    Clear
                                 </Button>
-                                <textarea rows="20" id="textEntry" onChange={handleTextChange} value={text} />
+                                    <textarea
+                                        style={ textareaStyle }
+                                        rows='20'
+                                        id="textEntry"
+                                        onChange={handleTextChange}
+                                        value={text}
+                                        aria-label='text entry'
+                                    />
                             </Col>
                         </Row>
                     </div>
