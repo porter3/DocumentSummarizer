00ee0db8384f71f151bc74a5e36308ef51ef531e
porter3 <jakeporter310@gmail.com>
2020-08-13 13:00:40 -0400

    edit toString()

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/domainEntities/SummaryComponents.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/domainEntities/SummaryComponents.java
@@ -14,9 +14,6 @@ public class SummaryComponents {
 
     @Override
     public String toString() {
-        return "SummaryComponents{" +
-                "sentences=" + sentences +
-                ", summaryCount=" + summaryCount +
-                '}';
+        return "{ " + "Sentence count: " + sentences.size() + "Summary count: " + summaryCount + " }";
     }
 }
