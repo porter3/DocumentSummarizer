de22531fa26c68dff47cbde6bf777a283dbbf1a2
porter3 <jakeporter310@gmail.com>
2020-08-18 16:16:39 -0400

    break coupling to specific script
    add consolidateProcessArgs(), get file name programatically in writeScriptToTempFile(), make args for runPythonScript() and initiateScriptProcess() 'String...' instead of a few specific Strings

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/scriptRunners/JARScriptRunner.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/scriptRunners/JARScriptRunner.java
@@ -1,14 +1,16 @@
 package com.jakeporter.DocumentSummarizer.utilities.scriptRunners;
 
 import com.jakeporter.DocumentSummarizer.exceptions.PythonScriptException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 
-// TODO: make this class more generic and have a subclass extend more specific functionality
+
 public class JARScriptRunner {
 
     private final String pythonCmd;
-    private final String script;
+    private final String script; // name of file in src/main/resources/
 
     private File scriptFile;
 
@@ -17,10 +19,12 @@ public class JARScriptRunner {
         this.script = script;
     }
 
-    public String runPythonScript(String text, String attributeDelimiter, String sentenceDelimiter, String summaryCountDelimiter) {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public String runPythonScript(String text, String... processArgs) {
         String result = null;
         try {
-            Process pythonProcess = initiateScriptProcess(attributeDelimiter, sentenceDelimiter, summaryCountDelimiter);
+            Process pythonProcess = initiateScriptProcess(processArgs);
             writeToStdIn(pythonProcess, text);
             result = getResultFromStdOut(pythonProcess);
         } catch (Exception e) {
@@ -31,9 +35,10 @@ public class JARScriptRunner {
         return result;
     }
 
-    private Process initiateScriptProcess(String attributeDelimiter, String sentenceDelimiter, String summaryCountDelimiter) {
+    private Process initiateScriptProcess(String... processArgs) {
         this.scriptFile = writeScriptToTempFile();
-        ProcessBuilder processBuilder = new ProcessBuilder(pythonCmd, scriptFile.getAbsolutePath(), attributeDelimiter, sentenceDelimiter, summaryCountDelimiter);
+        String[] allProcessArgs = consolidateProcessArgs(pythonCmd, scriptFile.getAbsolutePath(), processArgs);
+        ProcessBuilder processBuilder = new ProcessBuilder(allProcessArgs);
         processBuilder.redirectErrorStream(true);
         Process pythonProcess;
         try {
@@ -43,6 +48,16 @@ public class JARScriptRunner {
             throw new PythonScriptException("Something went wrong running the Python script.");
         }
         return pythonProcess;
+    }
+
+    private String[] consolidateProcessArgs(String pythonCmd, String scriptPath, String[] otherArgs) {
+        String[] processArgs = new String[otherArgs.length + 2];
+        processArgs[0] = pythonCmd;
+        processArgs[1] = scriptPath;
+        for (int i = 0; i < otherArgs.length; i++) {
+            processArgs[i + 2] = otherArgs[i];
+        }
+        return processArgs;
     }
 
     private void writeToStdIn(Process process, String text) {
@@ -90,9 +105,10 @@ public class JARScriptRunner {
 
     private File writeScriptToTempFile() {
         InputStream scriptStream = JARScriptRunner.class.getClassLoader().getResourceAsStream(script);
+        String[] filenameComponents = script.split("\\.");
         File scriptFile;
         try {
-            scriptFile = File.createTempFile("textSummarizer", ".py");
+            scriptFile = File.createTempFile(filenameComponents[0], filenameComponents[1]);
             byte[] buffer = new byte[scriptStream.available()];
             scriptStream.read(buffer);
             scriptStream.close();
