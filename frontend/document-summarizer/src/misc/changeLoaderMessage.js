6dd137f1af0ace2d989e959255731d5fee1e68da
porter3 <jakeporter310@gmail.com>
2020-08-19 16:25:26 -0400

    return array of the timeouts' IDs

--- frontend/document-summarizer/src/misc/changeLoaderMessage.js
+++ frontend/document-summarizer/src/misc/changeLoaderMessage.js
@@ -1,9 +1,10 @@
 import getLoaderMessage from './getLoaderMessage'
 
 export default function changeLoaderMessage(setLoaderMessage, fileSizeInBytes) {
-    setTimeout(() => { setLoaderMessage(getLoaderMessage(fileSizeInBytes)) }, 4000)
-    setTimeout(() => setLoaderMessage('Still working...'), 30000)
-    setTimeout(() => setLoaderMessage('STILL working...'), 60000)
-    setTimeout(() => setLoaderMessage("I hope you're not getting too impatient. However, you're the one that wanted to summarize something this long."), 80000)
-    setTimeout(() => setLoaderMessage('Still working...'), 100000)
+    const timeoutA = setTimeout(() => { setLoaderMessage(getLoaderMessage(fileSizeInBytes)) }, 4000)
+    const timeoutB = setTimeout(() => setLoaderMessage('Still working...'), 30000)
+    const timeoutC = setTimeout(() => setLoaderMessage('STILL working...'), 60000)
+    const timeoutD = setTimeout(() => setLoaderMessage("I hope you're not getting too impatient. However, you're the one that wanted to summarize something this long."), 80000)
+    const timeoutE = setTimeout(() => setLoaderMessage('Still working...'), 100000)
+    return [ timeoutA, timeoutB, timeoutC, timeoutD, timeoutE ]
 }
