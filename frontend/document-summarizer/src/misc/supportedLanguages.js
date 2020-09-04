412db9423dc02525a64ef27ec5aaa5bf73746157
porter3 <jakeporter310@gmail.com>
2020-09-04 18:46:06 -0400

    add select items for arabic, danish, finnish, german

--- frontend/document-summarizer/src/misc/supportedLanguages.js
+++ frontend/document-summarizer/src/misc/supportedLanguages.js
@@ -1,7 +1,23 @@
 const supportedLanguages = [
     {
+        symbol: 'ar',
+        word: 'Arabic'
+    },
+    {
+        symbol: 'da',
+        word: 'Danish'
+    },
+    {
         symbol: 'en',
         word: 'English (US)'
+    },
+    {
+        symbol: 'fi',
+        word: 'Finnish'
+    },
+    {
+        symbol: 'de',
+        word: 'German'
     },
     {
         symbol: 'es',
