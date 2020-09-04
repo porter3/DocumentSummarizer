22a7404d0682a6651029a734d46eb4872623fe8d
porter3 <jakeporter310@gmail.com>
2020-09-04 14:50:18 -0400

    add file to hold supported language objects

---
+++ frontend/document-summarizer/src/misc/supportedLanguages.js
@@ -0,0 +1,12 @@
+const supportedLanguages = [
+    {
+        symbol: 'en',
+        word: 'English (US)'
+    },
+    {
+        symbol: 'es',
+        word: 'Spanish'
+    }
+]
+
+export default supportedLanguages
