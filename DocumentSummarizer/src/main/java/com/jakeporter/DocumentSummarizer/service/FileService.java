1b98ef02350647c006b09894feacf0558c7bb7e6
porter3 <jakeporter310@gmail.com>
2020-09-06 14:29:47 -0400

    remove AWS3Client dependency

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/service/FileService.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/service/FileService.java
@@ -1,11 +1,9 @@
 package com.jakeporter.DocumentSummarizer.service;
 
-import com.amazonaws.services.rekognition.model.Summary;
 import com.jakeporter.DocumentSummarizer.domainEntities.SummaryComponents;
 import com.jakeporter.DocumentSummarizer.exceptions.*;
 import com.jakeporter.DocumentSummarizer.utilities.fileUtils.FileInfoGetter;
 import com.jakeporter.DocumentSummarizer.utilities.fileUtils.uploaders.AWSS3Client;
-import com.jakeporter.DocumentSummarizer.utilities.summarizers.DocumentSummarizer;
 import com.jakeporter.DocumentSummarizer.utilities.summarizers.PythonSummarizer;
 import com.jakeporter.DocumentSummarizer.utilities.textExtractors.*;
 import com.jakeporter.DocumentSummarizer.utilities.validators.PythonValidator;
@@ -16,15 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 @Service
 public class FileService {
-
-    @Autowired
-    private AWSS3Client awsClient;
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -35,43 +31,34 @@ public class FileService {
         BigDecimal maxSize = new BigDecimal(FIVE_MB_IN_BYTES);
         BigDecimal mbInBytes = new BigDecimal(MB_IN_BYTES);
         BigDecimal fileSize = new BigDecimal(String.valueOf(file.getSize()));
-        if (fileSize.compareTo(maxSize) == 1) {
+        if (fileSize.compareTo(maxSize) > 0) {
             throw new FileTooLargeException("File size cannot exceed " + maxSize.divide(mbInBytes, RoundingMode.FLOOR) + " MB. Your file is " + fileSize.divide(mbInBytes).setScale(2, RoundingMode.FLOOR) + " MB.");
         }
     }
 
-    public String uploadFile(MultipartFile file) {
-        String fileUrl;
-        // this is just here to throw an exception early on if the file format is unsupported
-        try {
-            new FileInfoGetter().getFileType(FilenameUtils.getExtension(file.getOriginalFilename()));
-        } catch (UnsupportedFileFormatException e) {
-            e.printStackTrace();
-            throw new UnsupportedFileFormatException(e.getMessage());
-        }
-        try {
-            fileUrl = awsClient.uploadFile(file);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new FileUploaderException(e.getMessage());
-        }
-        return fileUrl;
-    }
-
     /* There's an ugly chain of exception throws in here- they're specific because I want to send different status codes to the client for each specific problem
        and I have to re-throw them in here for the ExceptionHandlerController to intercept them */
-    public SummaryComponents summarize(MultipartFile mpFile, String fileUrl, String language) {
-        FileType fileType = new FileInfoGetter().getFileType(FilenameUtils.getExtension(mpFile.getOriginalFilename()));
-        InputStream s3ObjectStream;
-        try {
-            s3ObjectStream = awsClient.getS3ObjectInputStream(fileUrl);
-        } catch (FileUploaderException e) {
-            throw new FileUploaderException(e.getMessage());
-        }
+    public SummaryComponents summarize(MultipartFile mpFile, String language) {
         SummaryComponents components = null;
+        FileType fileType;
+        InputStream inputStream;
+        try {
+            fileType = new FileInfoGetter().getFileType(FilenameUtils.getExtension(mpFile.getOriginalFilename()));
+        } catch (FileException e) {
+            throw new FileException(e.getMessage());
+        }
+        try {
+            inputStream = mpFile.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new FileException("Something went wrong processing the file.");
+        }
         try {
             FileTextExtractor extractor = FileTextExtractorFactory.getExtractor(fileType);
-            components = new PythonSummarizer(extractor, new PythonValidator()).summarize(s3ObjectStream, language);
+            long start = System.currentTimeMillis();
+            components = new PythonSummarizer(extractor, new PythonValidator()).summarize(inputStream, language);
+            double executionTime = (System.currentTimeMillis() - start) / 1000D;
+            logger.info("-- Execution time (sec): " + executionTime);
             logger.info("Summary components: " + components.toString());
         } catch (TextExtractorException e) {
             e.printStackTrace();
@@ -84,8 +71,6 @@ public class FileService {
             throw new PythonScriptException(e.getMessage());
         } catch (Exception e) { // Don't know what uncaught exceptions could throw this at the moment, but I want to ensure the deletion of any uploaded files
             e.printStackTrace();
-        } finally {
-            awsClient.deleteFileFromS3Bucket(fileUrl);
         }
         return components;
     }
