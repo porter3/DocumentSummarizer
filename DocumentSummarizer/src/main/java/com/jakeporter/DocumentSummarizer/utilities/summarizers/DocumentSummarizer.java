f4a74357bd050d7ace063093dbc8b9360c6ed1b3
porter3 <jakeporter310@gmail.com>
2020-09-06 14:35:41 -0400

    run validateText() (in template methods) asynchronously

--- DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/summarizers/DocumentSummarizer.java
+++ DocumentSummarizer/src/main/java/com/jakeporter/DocumentSummarizer/utilities/summarizers/DocumentSummarizer.java
@@ -4,15 +4,12 @@ import com.jakeporter.DocumentSummarizer.domainEntities.SummaryComponents;
 import com.jakeporter.DocumentSummarizer.exceptions.SummaryException;
 import com.jakeporter.DocumentSummarizer.utilities.textExtractors.FileTextExtractor;
 import com.jakeporter.DocumentSummarizer.utilities.validators.TextValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 
 
 public abstract class DocumentSummarizer {
 
-    private static final int CHAR_MAX = Integer.MAX_VALUE; // prevent unpredictable behavior
     private FileTextExtractor extractor;
     private TextValidator validator;
 
@@ -25,31 +22,38 @@ public abstract class DocumentSummarizer {
         this.validator = validator;
     }
 
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-
     // template method for files
     public SummaryComponents summarize(InputStream stream, String language) {
         String text = extractor.extractTextFromStream(stream);
-        checkTextLength(text);
-        validator.validateText(text, language);
-        return computeSummaries(text, language);
+        Thread validatorThread = new Thread(() -> {
+            validator.validateText(text, language);
+        });
+        validatorThread.start();
+        SummaryComponents components = computeSummaries(text, language);
+        try {
+            validatorThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new SummaryException("Something went wrong.");
+        }
+        return components;
     }
 
-    // template method for pure text
+    // template method for non-file text
     public SummaryComponents summarize(String text, String language) {
-        checkTextLength(text);
-        validator.validateText(text, language);
-        return computeSummaries(text, language);
+        Thread validatorThread = new Thread(() -> {
+            validator.validateText(text, language);
+        });
+        validatorThread.start();
+        SummaryComponents components = computeSummaries(text, language);
+        try {
+            validatorThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new SummaryException("Something went wrong.");
+        }
+        return components;
     }
 
     protected abstract SummaryComponents computeSummaries(String text, String language);
-
-    private void checkTextLength(String text) {
-        int textLength = text.length();
-        logger.info("Text length: " + textLength);
-        if (textLength > CHAR_MAX) {
-            throw new SummaryException("The current character count limit for texts to be summarized is " + CHAR_MAX + ".\\nYour character count: " + textLength);
-        }
-    }
-
 }
