7e3c0d6aac5554b1a7724809ad0366af54adf70d
porter3 <jakeporter310@gmail.com>
2020-10-05 15:23:52 -0400

    add <div>s around loader and loader message for flex container creation

--- frontend/document-summarizer/src/components/Loader.js
+++ frontend/document-summarizer/src/components/Loader.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
 import '../styling/css/loader.css'
 
 export default function Loader({ loaderMessage }) {
     return (
         <>
-            <div className="reverse-spinner"></div>
+            <div className='spinnerRow'>
+                <div className="reverse-spinner"></div>
+            </div>
             {loaderMessage &&
-                <div id='loaderMessage'>{loaderMessage}</div>
+                <div className='messageRow'>
+                    <Typography variant='h5'>{loaderMessage}</Typography>
+                </div>
             }
         </>
     )
