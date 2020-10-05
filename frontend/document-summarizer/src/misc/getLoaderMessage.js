ba808af0f7e1539e2cfd8240f81669e4fca70af9
porter3 <jakeporter310@gmail.com>
2020-10-05 15:25:17 -0400

    change message

--- frontend/document-summarizer/src/misc/getLoaderMessage.js
+++ frontend/document-summarizer/src/misc/getLoaderMessage.js
@@ -2,7 +2,7 @@ export default function getLoaderMessage(sizeInBytes) {
     const sizeInKB = sizeInBytes / 1000
     let message = ''
     if (sizeInKB > 3000) {
-        message = "Are you trying to summarize War and Peace or something? This is going to take a few minutes."
+        message = "Wow, that's a lot of text. This is going to take a few minutes."
     } else if (sizeInKB > 1000) {
         message = 'Oof, this is a decent amount of text to summarize. This might take a couple minutes.'
     } else if (sizeInKB > 350) {
