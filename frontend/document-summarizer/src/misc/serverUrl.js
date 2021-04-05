06488020be5f341164e88e80a9c2056a2f8cb2cd
Jake Porter <jakeporter310@gmail.com>
2021-04-04 22:34:13 -0400

    update url as environment changed

--- frontend/document-summarizer/src/misc/serverUrl.js
+++ frontend/document-summarizer/src/misc/serverUrl.js
@@ -1,4 +1,4 @@
-const isLocalBuild = false
+const isLocalBuild = true
 const path = '/summarize'
-const serverUrl = isLocalBuild ? 'http://localhost:5000' : 'http://documentsummarizer-env.eba-5pbvjawt.us-east-1.elasticbeanstalk.com'
+const serverUrl = isLocalBuild ? 'http://localhost:5000' : 'http://jar-java11-0421.eba-5pbvjawt.us-east-1.elasticbeanstalk.com'
 export default serverUrl + path
